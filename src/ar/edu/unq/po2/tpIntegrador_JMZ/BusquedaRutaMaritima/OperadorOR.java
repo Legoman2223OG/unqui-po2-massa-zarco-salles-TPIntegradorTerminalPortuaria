fde96be69a75155e7c707ebb971387f5fee5ae8f
Julián Zarco <julianzarco.2003@outlook.com>
2025-11-04 19:32:51 -0300

    Actualizacion 4/11

---
+++ src/ar/edu/unq/po2/tpIntegrador_JMZ/BusquedaRutaMaritima/OperadorOR.java
@@ -0,0 +1,15 @@
+package ar.edu.unq.po2.tpIntegrador_JMZ.BusquedaRutaMaritima;
+
+import java.util.ArrayList;
+
+import ar.edu.unq.po2.tpIntegrador_JMZ.NavierasYCircuitos.Viaje;
+
+public class OperadorOR implements ComponenteBusqueda {
+
+	@Override
+	public ArrayList<Viaje> filtrar(ArrayList<Viaje> listaViajes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
