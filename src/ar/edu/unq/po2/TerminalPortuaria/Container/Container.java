79d67f5d00cd825b5fa84345eede48dbe8ebb05f
Fabrizio Lautaro Massa <massafabrizio123@gmail.com>
2025-11-09 15:30:54 -0300

    Arregle algunos packages pero igual hace falta terminal algunas cosas

--- src/ar/edu/unq/po2/TerminalPortuaria/Container/Container.java
+++ src/ar/edu/unq/po2/TerminalPortuaria/Container/Container.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import ar.edu.unq.po2.TerminalPortuaria.Terminal.Cliente;
+import ar.edu.unq.po2.TerminalPortuaria.Cliente.Cliente;
+
 
 /**
  * Una clase que representa un container para transportar cosas. Posee un ancho, largo y altura incluido su peso.
--- src/ar/edu/unq/po2/TerminalPortuaria/Container/IBillOfLanding.java
+++ src/ar/edu/unq/po2/TerminalPortuaria/Container/IBillOfLanding.java
@@ -2,7 +2,8 @@ package ar.edu.unq.po2.TerminalPortuaria.Container;
 
 import java.util.List;
 
-import ar.edu.unq.po2.TerminalPortuaria.Terminal.Cliente;
+import ar.edu.unq.po2.TerminalPortuaria.Cliente.Cliente;
+
 
 public interface IBillOfLanding {
 	public abstract List<Cliente> getDuenios();
